fb6ed9a85861e14f2bcb3578ef4d825b1273fe23
bpotter0772 <114099707+bpotter0772@users.noreply.github.com>
2024-11-27 20:18:52 -0500

    Revert "UI update"

--- Assets/Scripts/PlayerCam.cs
+++ Assets/Scripts/PlayerCam.cs
@@ -8,17 +8,12 @@ public class PlayerCam : MonoBehaviour
     private PlatformPlayer playerScript;
     private float currentSpeed;
     private bool dashing;
-
-    private float minSize = 13f;
-    private float maxSize; 
-    private float speedMult = 50f;
+    private bool dashingDeccel;
     private float sizeCheck;
 
     void Start() 
     {
         playerScript = GetComponent<PlatformPlayer>();
-        dashing = false;
-        maxSize = minSize * (1f + 25f/speedMult);
     }
 
     void Update()
@@ -26,37 +21,39 @@ public class PlayerCam : MonoBehaviour
         currentSpeed = playerScript.currentSpeed;
         cam.transform.position = transform.position + new Vector3(0,0,-10f);
 
-        sizeCheck = minSize * (1 + Mathf.Abs(currentSpeed)/speedMult);
+        sizeCheck = 15.0f * (1 + Mathf.Abs(currentSpeed)/70);
         
-        if(sizeCheck <= maxSize && !dashing) {
+        if(sizeCheck <= (1 + Mathf.Abs(25)/70) && !dashing && !dashingDeccel) {
             cam.orthographicSize = sizeCheck;
         }
         if(playerScript.dashing && playerScript.dashCount >= 0) {
             StartCoroutine(DashCameraBoost());
+        }
+        if(cam.orthographicSize >= 20.1f && !dashing) {
+            StartCoroutine(DashCameraDeccel());
         }
     }
 
     IEnumerator DashCameraBoost() 
     {
         dashing = true;
-        int count = 0;
         
-        while(count < 2) {
-            cam.orthographicSize += 3f;
-            
-            yield return new WaitForSeconds(0.02f);
-
-            count++;
-        }
-
-        while(count < 4) {
-            cam.orthographicSize -= 3f;
-
-            yield return new WaitForSeconds(0.02f);
-
-            count++;
+        while(cam.orthographicSize < 25.0f) {
+            cam.orthographicSize += 0.8f;
+            yield return new WaitForEndOfFrame();
         }
 
         dashing = false;       
+    }
+
+    IEnumerator DashCameraDeccel() {
+        dashingDeccel = true;
+        
+        while(cam.orthographicSize > sizeCheck) {
+            cam.orthographicSize -= 0.8f;
+            yield return new WaitForEndOfFrame();
+        }
+
+        dashingDeccel = false;  
     }
 }
